fedcbf930c2b9e20cceb83f1f7a0b27b8ef5acb7
Nadia3-05 <buynadia1997@gmail.com>
2022-08-22 23:20:45 +0300

    main

---
+++ HW2/js/hw2.js
@@ -0,0 +1,38 @@
+let minNumber ;
+let minRound;
+do {
+    minNumber = prompt ( "Введіть мінімальне число (N)");
+} while (!minNumber);
+// console.log({minNumber})
+minRound = Math.round(minNumber);
+console.log({minRound})
+
+let maxNumber;
+let maxRound;
+do {
+    maxNumber = prompt ( "Введіть максимальне число (M)");
+
+} while (!maxNumber);
+// console.log({maxNumber})
+maxRound = Math.round(maxNumber);
+console.log({maxRound})
+
+let counter = 0;
+let evenOdd = confirm("Пропускати парні числа ?");
+for (minRound; minRound <= maxRound; minRound++) {
+    let remainder = minRound % 2;
+    if (remainder === 1 && evenOdd === true) {
+        counter += minRound;
+        console.log(minRound);
+    } else if (evenOdd === false) {
+        counter += minRound;
+        console.log(minRound);
+    }}
+console.log("Sum: " +counter);
+
+let text = document.getElementById("HW2");
+text.innerHTML = 
+    `<p>Число N - <span>${minRound-maxRound}</span></p>
+    <p>Число M - <span>${maxRound}</span></p>
+    <p>Булеве значення - <span>${evenOdd}</span></p>
+    <p>Відповідь - <span>${counter}</span></p>`;
