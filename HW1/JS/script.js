f69e4a03360d9072e8605956df19803c2367bcb6
Nadia3-05 <buynadia1997@gmail.com>
2022-08-22 23:20:57 +0300

    main

--- HW1/JS/script.js
+++ HW1/JS/script.js
@@ -2,69 +2,71 @@ console.log('HW#1 - Variables');
 
 
 //призначення значень змінним//
-let priceItem1 = '15.678' , 
-    priceItem2 = '123.965' ,
-    priceItem3 = '90.2345' ;
-
+let price_apple = '15.678' , 
+    price_fish = '123.965' ,
+    price_milk = '90.2345' ;
+    console.log('Price apple: '+price_apple +'$');
+    console.log('Price fish: '+price_fish +'$ ');
+    console.log('Price milk: '+price_milk +'$');
 //Максимальне значення//
-let max = Math.max( priceItem1,  priceItem2,  priceItem3);
-console.log('Максимальне значення: '+max +'грн ');
+let max = Math.max( price_apple,  price_fish,   price_milk);
+console.log('Max mean: '+max +'$');
 
 //Мінімальне значення//
-let min = Math.min( priceItem1,  priceItem2,  priceItem3);
-console.log('Мінімальне значення: '+min+'грн ');
+let min = Math.min( price_apple,  price_fish,  price_milk);
+console.log('Min mean: '+min+'$ ');
 
 //Сума всіх значень//
-let sum = ( +priceItem1 + +priceItem2 + +priceItem3);
-console.log('Сума: '+sum+'грн ');
+let sum = ( +price_apple + +price_fish + +price_milk);
+console.log('Sum: '+sum+'$ ');
 
 //Сума завкруглень//
-let price1 = Math.floor(priceItem1)
-console.log('Ціна завкруглення 1 товару: ' +price1 +'грн ' );
-let price2 = Math.floor(priceItem2)
-console.log('Ціна завкруглення 2 товару: ' +price2 +'грн ' );
-let price3 = Math.floor(priceItem3)
-console.log('Ціна завкруглення 3 товару: ' +price3 +'грн ' );
+let price1 = Math.floor(price_apple)
+console.log('Price of 1 product is ronded floor: ' +price1 +'$' );
+let price2 = Math.floor(price_fish)
+console.log('Price of 2 product is ronded floor: ' +price2 +'$ ' );
+let price3 = Math.floor(price_milk)
+console.log('Price of 3 product is ronded floor: ' +price3 +'$ ' );
 let sum_all = ( +price1 + +price2 + +price3);
-console.log('Сума завкруглень: '+sum_all +'грн ');
+console.log('Sum of floor prices: '+sum_all +'$ ');
 
 //Сума завкруглень товарів до сотень//
 price_100_number1 = '0';
 let number1 = 15;
 if(number1 < 50){
-    console.log('Ціна завкруглення 1 товару до сотень: ' +price_100_number1 +'грн ')
+    console.log('Price 1 product rounded to 100 : ' +price_100_number1 +'$ ')
 } else{
     console.log('100')
 }
 price_100_number2 = '100';
 let number2 = 123;
 if(number2 < 150 ){
-    console.log('Ціна завкруглення 2 товару до сотень: ' +price_100_number2 +'грн ')
+    console.log('Price 2 product rounded to 100: ' +price_100_number2 +'$ ')
 } else{
     console.log('200')
 }
 price_100_number3 = '100';
 let number3 = 90;
 if(number3 < 150 ){
-    console.log('Ціна завкруглення 3 товару до сотень: ' +price_100_number3 +'грн ')
+    console.log('Price 3 product rounded to 100: ' +price_100_number3 +'$ ')
 } else{
     console.log('200')
 }
 let sum_price_100 = ( +price_100_number1 + +price_100_number2 + +price_100_number3);
-console.log('Сума завкруглень товарів до сотень: '+sum_price_100 +'грн ');
+console.log('Sum rounded to 100: '+sum_price_100 +'$ ');
 
 //перевірка на парність//
-sum_all = 'парне число';
-sum_all = 228;
-if(sum_all % 2 ==0){
-    console.log(sum_all +' - це парне число')
+sum_alll = 'true';
+let Numberr = 228;
+if(Numberr % 2 ==0){
+    console.log(sum_alll +' - even number:' +' true')
 } else{
-    console.log(sum_all +' - це непарне число')
+    console.log(sum_alll +' - even number:' +' false')
 }
 
 //решта з 500грн//
 remainder = 500 - sum;
-console.log('Решта з 500грн - ' +remainder +' грн')
+console.log('Remainder ' +remainder +' $')
 
 //середнє арифм цін//
 var arr = [15.68 , 123.97 , 90.23];
@@ -73,33 +75,38 @@ for (var i = 0; i < arr.length; i++) {
     summ +=arr[i];
 }
 var result = summ / arr.length;
-console.log('Середнє арифм цін: ' +result)
+console.log('Arithmetic mean: ' +result)
 
 //знижка//
 let sale = Math.floor(Math.random() * 10);
-console.log('Випадкова знижка - ' +sale +'%')
+console.log('Random sale - ' +sale +'%')
+
 
 //Сума із знижкою//
-sale = (sum / 100 )* sale;
-let TOTAL = sum - sale;
-console.log('Сума із знижкою: ' +TOTAL.toFixed(2) +'грн')
+let sales = (sum / 100 )* sale;
+let TOTAL = sum - sales;
+console.log('Total: ' +TOTAL.toFixed(2) +'$')
 
 //прибуток//
-let profit = (sum / 2) - sale;
-console.log('Прибуток: ' +profit +'грн')
+let profit = (sum / 2) - sales;
+console.log('Profit: ' +profit +'$')
+
 let html = `
-<p> max mean = ${max}</p>
-<p> min mean = ${min}</p>
-<p> sum of prices = ${sum}</p>
-<p> sum of floor prices = ${sum_all}</p>
-<p> Sum rounded to 100 = ${sum_price_100}</p>
-<p> Even/Odd number = ${sum_all}</p>
-<p> Remainder = ${remainder}</p>
-<p> Arithmetic mean = ${result}</p>
-<p> Random sale = ${sale}</p>
-<p> Total = ${TOTAL}</p>
-<p> Profit = ${profit}</p>`;
+<p> Price apple: = ${price_apple}$</p>
+<p> Price fish: = ${price_fish}$</p>
+<p> Price milk: = ${price_milk}$</p>
+<p> Max mean = ${max}$</p>
+<p> Min mean = ${min}$</p>
+<p> Sum of prices = ${sum}$</p>
+<p> Sum of floor prices = ${sum_all}$</p>
+<p> Sum rounded to 100 = ${sum_price_100}$</p>
+<p> Even number: 228 = ${sum_alll}</p>
+<p> Remainder = ${remainder}$</p>
+<p> Arithmetic mean = ${result}$</p>
+<p> Random sale = ${sale} %</p>
+<p> Total = ${TOTAL}$</p>
+<p> Profit = ${profit}$</p>`;
 
     
-document.getElementById("info").innerHTML = html;
+document.getElementById("hw1").innerHTML = html;
 
