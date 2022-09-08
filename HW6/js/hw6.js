9200690969cc38bde0fbfc138bc12562393b17f7
Nadia3-05 <buynadia1997@gmail.com>
2022-09-08 16:41:22 +0300

    main

--- HW6/js/hw6.js
+++ HW6/js/hw6.js
@@ -33,11 +33,65 @@ let arr = [];
     return arr;
     }
 console.log(getSubjects(students[0]));
+//2 
+function getAverageMark(students){
+  let averageMark = [];
+  let averageMarkLenght = 0;
+  for (let i in students.subjects) {
+      averageMark = Object.values(students.subjects)
+          .flat()
+          .reduce((a, b) => a + b);
+      averageMarkLenght = Object.values(students.subjects)
+          .flat().length;
+  }
+  return (averageMark / averageMarkLenght).toFixed(2);
+}
+console.log(getAverageMark(students[0]));
+//3
+function getStudentInfo(students){
+  return {
+  course : students.course,
+  name : students.name,
+  average : getAverageMark(students),};
+}
+console.log(getStudentInfo(students[0]));
+//4
+function getStudentsNames(student) {
+  return students.reduce((prev, item, index) => {
+    prev[index] = item.name;
+    return prev;
+  }, []).sort();
+}
+console.log(getStudentsNames(students[0]))
+//5
+function getBestStudent(students) {
+  let theBestStudent = students.reduce((a, b) =>
+      getAverageMark(a) > getAverageMark(b) ? a : b).name;
+  return theBestStudent;
+}
+console.log(getBestStudent(students));
+// let max = Math.max( getAverageMark(students[0]),  getAverageMark(students[1]),   getAverageMark(students[2]));
+// console.log(max);
+//6
+function calcRepeatLetter(initStr) {
+  const strItems = initStr.split('');
+  const result = {};
+  strItems.forEach( item => {
+    const normalItem = item.toLowerCase();
+    if(normalItem in result) {
+      result[normalItem] = result[normalItem] + 1;
+    } else {
+      result[normalItem] = 1;
+    }
+  });
+  return result}
+console.log(calcRepeatLetter('Test'))
 
-//2
-// getAverageMark(students[0]) --> 3.79
-console.log(Object.values(students[0].subjects));
-    
-      
-
-  
+const l = document.getElementById("HW6");
+  l.innerHTML = 
+    `<p>Function1 - Subjects: ${getSubjects(students[0])}</p>
+    <p>Function2 - Average Mark: ${getAverageMark(students[0])}</p>
+    <p>Function3 - Student Info: ${getStudentInfo(students[0])}</p>
+    <p>Function4 - Students Names: ${getStudentsNames(students[0])}</p>
+    <p>Function5 - Best Student: ${getBestStudent(students)}</p>
+    <p>Function6 - Calculate Repeat Letter: ${calcRepeatLetter('Test')}</p>`;
