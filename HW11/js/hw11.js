aa8939910fa8e1d3d55ba52ca4ee145acf8ebf31
Nadia3-05 <buynadia1997@gmail.com>
2022-09-25 22:14:18 +0300

    main

---
+++ HW11/js/hw11.js
@@ -0,0 +1,8 @@
+function playNote(e){
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+
+    if(!key) return;
+    audio.play();
+}
+window.addEventListener('keydown', playNote);
