16c6af5eb11772bab7444ecbadcc1449aa9f3c68
Nadia3-05 <buynadia1997@gmail.com>
2022-10-07 22:26:24 +0300

    main

--- HW14/js/hw14.js
+++ HW14/js/hw14.js
@@ -9,51 +9,32 @@ console.log(idGenerator.next().value);
 console.log(idGenerator.next().value);
 console.log(idGenerator.next().value);//викликаємо потрібну кількість разів
 
+
 // Task 2
-// let  counter = 14;
-// document.getElementById("bntUp").addEventListener('click',changeSizeUp);
-// document.getElementById("bntDown").addEventListener('click',changeSizeDown);
+const text = document.querySelector(".text");
+const size = parseInt(window.getComputedStyle(text).fontSize);
 
-// function changeSizeUp () {
-//     counter++;
-//     document.getElementById("text").innerHTML = counter;    
-//     document.getElementById("text").setAttribute('style', `font-size: ${counter}pt`);  
-// }
-// function changeSizeDown () {
-//     counter--;
-//     document.getElementById("text").innerHTML = counter;    
-//     document.getElementById("text").setAttribute('style', `font-size: ${counter}pt`);   
-// }
-function* newFontGenerator() {
-    yield* showSizeUp
-    yield* showSizeDown
-}
-function* main(){
-    const uxIterator = UXDesignFlow();
-    uxIterator.next();
-
-    sizeUp.on('click', () =>{
-        let i = 14
-            while( i < 102)
-            i++;
-        // for(let i = 14; i < 72; i++) {
-        //     yield i++
-        // }   
-        uxIterator.next();
-    });
-
-    sizeDown.on('click', () =>{
-        let i = 14
-        while( i < 102)
-        i--;
-        // for(let i = fontGenerator.next().value ; i < 72; i--) {
-        //     yield i--
-        // }   
-        uxIterator.next();
-    })
-
-}
-
-
-
+function* newFontGenerator(seed) {
+    let value = seed;
+    let counter;
+    while (true) {
+      if (counter === "UP") {
+        value = value + 2;
+        counter = yield value;   
+      } else  if(counter === "DOWN") {
+        value = value - 2;
+        counter = yield value;
+      } else {
+        counter = yield value;
+      }
+    } 
+  };
 const fontGenerator = newFontGenerator(14);
+
+document.querySelector("#bntUP").addEventListener("click", () => {
+    text.style.fontSize = `${fontGenerator.next("UP").value}px`;    
+})
+
+document.querySelector("#bntDOWN").addEventListener("click", () => {
+    text.style.fontSize = `${fontGenerator.next("DOWN").value}px`;
+})
